f1866b7800d80fe544727f821acd8f10bd69aaaa
Daniel Gorbunov <38931252+dgorbunov@users.noreply.github.com>
2020-06-26 18:46:20 -0400

    Update sensitivity

--- CapacitiveTouchTest/CapacitiveTouchTest.ino
+++ CapacitiveTouchTest/CapacitiveTouchTest.ino
@@ -1,17 +1,18 @@
 //Written by Daniel Gorbunov based off of Capacitive Sensing Library Example Code by Paul Badger
-//includes auto calibration in setup and variable sensitivity. I used a 470K resistor on ESP8266 (has to be under 500K) or 20M on an Uno (can sense up to a couple inches).
+//includes auto calibration in setup and variable sensitivity. I used a 470K resistor on ESP8266 or 20M on an Uno.
 
 
 #include <CapacitiveSensor.h>
 
-#define sens 4 //sensitivity (product factor, must be >1, higher = less sensitive)
+#define sens 6 //sensitivity (product factor, must be >1, HIGHer = more sensitive)
+#define sample 60  //sample size, default 30
 
 long thres = 0;
 
 CapacitiveSensor   touchSensor = CapacitiveSensor(D6,D7);        // 470K resistor between first pin (after resistors) and second pin (before resistors, direct to sensor)
 
 void setup(){
-   delay(100);
+   delay(2000);
    //cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);
    pinMode(LED_BUILTIN, OUTPUT);
@@ -21,11 +22,11 @@ void setup(){
 
 void loop(){
     long start = millis();
-    long reading =  touchSensor.capacitiveSensor(30);
+    long reading =  touchSensor.capacitiveSensor(sample);
 
-    Serial.print(start);
-    Serial.print(" ");
-    Serial.println(reading);
+//    Serial.print(start);
+//    Serial.print(" ");
+//    Serial.println(reading);
 
     if (reading > thres) {
       digitalWrite(LED_BUILTIN, LOW); //inverted on my esp8266, this turns led on
@@ -45,7 +46,7 @@ void calibrateTouch(int readings){
   long total = 0;
   int i = 0;
   for(i; i < readings; i++){
-    total += touchSensor.capacitiveSensor(30);
+    total += touchSensor.capacitiveSensor(sample);
   }
   Serial.print("iterations: ");
   Serial.println(i);
