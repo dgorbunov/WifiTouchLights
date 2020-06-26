e93c6021237ca2f2a6e48c5bb52121e5082c774e
Daniel Gorbunov <38931252+dgorbunov@users.noreply.github.com>
2020-06-26 12:58:46 -0400

    Update CapTouchTest for ESP8266

--- CapacitiveTouchTest/CapacitiveTouchTest.ino
+++ CapacitiveTouchTest/CapacitiveTouchTest.ino
@@ -1,39 +1,55 @@
 //Written by Daniel Gorbunov based off of Capacitive Sensing Library Example Code by Paul Badger
-//includes auto calibration in setup and variable sensitivity. I used a 20M resistor.
+//includes auto calibration in setup and variable sensitivity. I used a 470K resistor on ESP8266 (has to be under 500K) or 20M on an Uno (can sense up to a couple inches).
 
 
 #include <CapacitiveSensor.h>
 
-#define sens 5 //sensitivity (product factor, must be >1)
+#define sens 4 //sensitivity (product factor, must be >1, higher = less sensitive)
 
 long thres = 0;
 
-CapacitiveSensor   cs_4_5 = CapacitiveSensor(4,5);        // 20M resistor between pins 4 (after resistors) and 5 (before resistors, direct to sensor)
+CapacitiveSensor   touchSensor = CapacitiveSensor(D6,D7);        // 470K resistor between first pin (after resistors) and second pin (before resistors, direct to sensor)
 
 void setup(){
+   delay(100);
    //cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);
    pinMode(LED_BUILTIN, OUTPUT);
-   calibrateTouch(20);
+   calibrateTouch(100);
+   digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop(){
     long start = millis();
-    long reading =  cs_4_5.capacitiveSensor(30);
+    long reading =  touchSensor.capacitiveSensor(30);
 
+    Serial.print(start);
+    Serial.print(" ");
     Serial.println(reading);
 
-    if (reading > thres) digitalWrite(LED_BUILTIN, HIGH);
-    else digitalWrite(LED_BUILTIN, LOW);
+    if (reading > thres) {
+      digitalWrite(LED_BUILTIN, LOW); //inverted on my esp8266, this turns led on
+      Serial.print("touched: ");
+      Serial.println(reading);
+      Serial.print("thres: ");
+      Serial.println(thres);
+    }
+    else if (reading < thres) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
 
     delay(10);                             // arbitrary delay to limit data to serial port 
 }
 
 void calibrateTouch(int readings){
   long total = 0;
-  for(int i = 0; i < readings; i++){
-    total += cs_4_5.capacitiveSensor(30);
+  int i = 0;
+  for(i; i < readings; i++){
+    total += touchSensor.capacitiveSensor(30);
   }
+  Serial.print("iterations: ");
+  Serial.println(i);
   thres = (total / readings) * sens;
-  
+  Serial.println("thres: ");
+  Serial.println(thres);
 }
